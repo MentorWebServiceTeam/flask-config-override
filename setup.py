f091e8234602f1be19ce9ae759d0e00bd6239ec4
Josh Frankamp <frankamp@gmail.com>
2015-05-11 13:39:42 -0700

    pushing test reqs

--- setup.py
+++ setup.py
@@ -8,7 +8,13 @@ from setuptools import setup, find_packages
 def readme():
     with open('README.rst') as f:
         return f.read()
-test_requirements = ['flask']
+
+test_requirements = [
+    'flask==0.10.1',
+    'nose==1.3.6',
+    'coverage==3.7.1'
+]
+
 setup(name='config_override',
       version='0.1',
       description='Python utilities for loading config',
